f33571cc9e37bb673f6f748b62a6ed42e09b7040
charlieflowers <cflowers@gmail.com>
2014-05-10 13:37:56 -0400

    sweet, fully understood the lifetime error message, and fixed it properly. The lesson is, whatever you pass INTO as_slice must live as long as you want the RESULT from as_slice to live. You actually fix this error by making your INPUT ARGUMENT live longer.

--- warm-up.rs
+++ warm-up.rs
@@ -4,10 +4,12 @@ fn main() {
     use std::io::File;
     use std::str;
 
-    let file_bytes = File::open(&Path::new("charlie-to-parse.txt")).read_to_end();
+    let file_bytes = File::open(&Path::new("charlie-to-parse.txt")).read_to_end().unwrap();
 
-    let contents = file_bytes.unwrap().as_slice();
-    let string_contents = str::from_utf8(contents);
+    let contents = file_bytes.as_slice();
+    println!("The slice is {}", contents);
+
+    let string_contents = str::from_utf8(contents).unwrap();
 
 
 
