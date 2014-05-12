a7b3e6140c003e0e87d61d11d8733dd097028590
charlieflowers <cflowers@gmail.com>
2014-05-12 14:08:34 -0400

    closing in on my first token

--- warm-up.rs
+++ warm-up.rs
@@ -34,5 +34,40 @@ fn main() {
         println!("Next char is {}", next_char);
     }
 
+    let mut index = 0;
 
+    loop {
+        if index >= string_contents.len()  { break; }
+        let next_char = string_contents[index] as char;
+        println!("char {} is {}.", next_char, index);
+
+        let token = match next_char {
+            num if num.is_digit() => get_number(string_contents, index),
+            op if op == '+' || op == '-' => get_operator(string_contents, index, op),
+            _ => {fail!("unable to match char {} at index {}", next_char, index); "failed".to_owned() }
+        };
+
+        println!("Got token: {}", token);
+
+        index = index + 1;
+    }
+
+    println!("Congrats on using loop successfully.");
+}
+
+fn get_number(string_contents : &str, mut index : uint) -> ~str{
+    let mut value = "".to_owned();
+    loop {
+        let ch = string_contents[index] as char;
+        if ch.is_whitespace() { return "Number: ".to_owned() + value; }
+        if ! ch.is_digit() { fail!("Found a {} right in the middle of an expected number. Can't do that.", ch)}
+        value = value + ch;
+        index = index + 1;
+        if index >= string_contents.len() { fail!("Inside get_number, we ran past end of parser input and were planning to keep going.");}
+    }
+}
+
+fn get_operator(string_contents : &str, mut index : uint, operator_char : char) -> ~str {
+    fail!("get_operator not impleemnted yet, but index is {} and char is {}", index, operator_char);
+    "failed".to_owned()
 }
