38e5adb9e779bfe898396e034496ef5323fa213b
charlieflowers <cflowers@gmail.com>
2014-06-25 15:01:19 -0400

    * warm-up.rs: transmogrifying

--- warm-up.rs
+++ warm-up.rs
@@ -1,4 +1,28 @@
 // This is just me warming up with rust.
+use std::fmt;
+
+#[deriving(Show)]
+enum TokenTag {
+    Number,
+    Whitespace,
+    Operator,
+    Herecomment,
+    Comment
+}
+
+#[deriving(Show)]
+struct Token {
+    tag: TokenTag,
+    value: ~str,
+    length: uint,
+    index: uint
+}
+
+impl Token {
+    fn new(tag: TokenTag, value: ~str, length: uint, index: uint) -> Token {
+        Token {tag:tag, value:value, length:length, index:index}
+    }
+}
 
 fn main() {
     use std::io::File;
@@ -10,25 +34,6 @@ fn main() {
     println!("The slice is {}", contents);
 
     let string_contents = str::from_utf8(contents).unwrap();
-
-    // let first_char = string_contents[0];
-
-    // println!("The first char is {}", first_char);
-
-    // let second_char = string_contents[1];
-    // println!("the second char is {}", second_char);
-
-    // println!("Hello! I'm going to parse {}", string_contents);
-
-    // match first_char as char {
-    //     '4' => println!("Yes, the first char matches '4'"),
-    //     _   => fail!("No, first char does not match '4'")
-    // }
-
-    // for index in range(0u, string_contents.len()) {
-    //     let next_char = string_contents[index] as char;
-    //     println!("Next char is {}", next_char);
-    // }
 
     let index : &mut uint = &mut 0;
 
@@ -50,7 +55,7 @@ fn main() {
     }
 }
 
-fn get_whitespace(string_contents : &str, index : &mut uint) -> ~str {
+fn get_whitespace(string_contents : &str, index : &mut uint) -> Token {
     let mut value = "".to_owned();
     loop {
         if *index >= string_contents.len() {break};
@@ -65,10 +70,10 @@ fn get_whitespace(string_contents : &str, index : &mut uint) -> ~str {
 
     if value.len() == 0  { fail!("You are not supposed to call get_whitespace unless you know you got some. But I found zero characters of whitespace.")}
 
-    return "Whitespace: ".to_owned() + value;
+    return Token::new(Whitespace, value, value.len(), *index - value.len() )
 }
 
-fn get_number(string_contents : &str, index : &mut uint) -> ~str{
+fn get_number(string_contents : &str, index : &mut uint) -> Token {
     let mut value = "".to_owned();
     loop {
         let ch = string_contents[*index] as char;
@@ -80,11 +85,11 @@ fn get_number(string_contents : &str, index : &mut uint) -> ~str{
     }
 }
 
-fn get_operator(string_contents : &str, index : &mut uint) -> ~str {
+fn get_operator(string_contents : &str, index : &mut uint) -> Token {
     let mut result = "".to_owned();
     loop {
         let ch = string_contents[*index] as char;
-        if ch.is_whitespace() { return "Result: ".to_owned() + result; }
+        if ch.is_whitespace() { return "Operator: ".to_owned() + result; }
         if ch != '+' && ch != '-' { fail!("I thought I was parsing an result, but I found this in it: {}", ch)}
         result = result + std::str::from_char(ch);
         *index = *index + 1;
@@ -92,7 +97,7 @@ fn get_operator(string_contents : &str, index : &mut uint) -> ~str {
     }
 }
 
-fn get_comment(string_contents : &str, index : &mut uint) -> ~str {
+fn get_comment(string_contents : &str, index : &mut uint) -> Token {
     let first_ch = string_contents[*index] as char;
     if first_ch != '#' { fail!("I thought I was parsing a comment, but it starts with this: {}", first_ch)}
     let next_ch = string_contents[*index + 1] as char;
@@ -107,21 +112,23 @@ fn get_comment(string_contents : &str, index : &mut uint) -> ~str {
     }
 }
 
-fn expect(string_contents : &str, index : &mut uint, expectation : &str) -> ~str {
+fn expect(string_contents : &str, index : &mut uint, expectation : &str) -> Token {
+    let my_slice = string_contents.slice_from(*index);
+    println!("expecting! At index {}, expecting {} from {}.", *index, expectation, my_slice);
+
+    if ! my_slice.starts_with(expectation) { fail!("At index {}, expected {} but got \r\n {}.", *index, expectation, string_contents.slice_from(*index))}
+
     let mut result = "".to_owned();
-    for n in range(0, expectation.len() - 1) {
-        let actual = string_contents[*index + n] as char;
-        let expected = expectation[n] as char;
-        if actual != expected {
-            fail!("At index {}, expected {} but got \r\n {}.", *index, expectation, string_contents.slice_from(*index));
-        }
+    // todo charlie of course its crazy to append char by char
+    for n in range(0, expectation.len()) {
+        let actual = my_slice[n] as char;
         result = result + std::str::from_char(actual);
         *index = *index + 1;
     }
     return result;
 }
 
-fn get_herecomment(string_contents : &str, index : &mut uint) -> ~str {
+fn get_herecomment(string_contents : &str, index : &mut uint) -> Token {
     let mut result = expect(string_contents, index, "###");
     // Just keep going no matter what, until you hit the end or find ###.
     loop {
@@ -136,4 +143,9 @@ fn get_herecomment(string_contents : &str, index : &mut uint) -> ~str {
         *index = *index + 1;
         if *index >= string_contents.len() { fail!("Inside get_herecomment, we ran past end of parser input and were planning to keep going. The herecomment token we have so far is {}", result);}
     }
+}
+
+#[test]
+fn whole_shebang_should_succeed() {
+    main();
 }
