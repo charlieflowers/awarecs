252fc1362c3e170115adc7b6ef18d3c6ed69d29b
charlieflowers <cflowers@gmail.com>
2014-07-26 18:17:03 -0400

    Phew! Got the overloading workaround working for both MagicNumber and Option<MagicNumber>

--- trait_experiment.rs
+++ trait_experiment.rs
@@ -10,6 +10,7 @@ pub struct MagicNumber {
 //     }
 // }
 
+// During this process, don't get confused. THERE MUST BE ONE AND ONLY ONE IMPLEMENTATION OF ADD!
 impl<R: CanBeAddedToMagicNumber>  Add<R, MagicNumber> for MagicNumber {
     fn add(&self, rhs: &R) -> MagicNumber {
         rhs.add_to_magic_number(self)
@@ -26,10 +27,23 @@ impl CanBeAddedToMagicNumber for MagicNumber {
     }
 }
 
+impl CanBeAddedToMagicNumber for Option<MagicNumber> {
+    fn add_to_magic_number(&self, lhs: &MagicNumber) -> MagicNumber {
+        if self.is_none() { return *lhs; }
+        lhs + self.unwrap()
+    }
+}
+
 fn main() {
     let one = MagicNumber { value: 40 };
     let two = MagicNumber { value: 2 };
     let result = one + two;
     println!("result: {}", result);
     assert_eq!(result.value, 42);
+
+    let three : Option<MagicNumber> = None;
+
+    let option_result = result + three;
+    println!("option result: {}", option_result);
+    assert_eq!(option_result.value, 42);
 }
