bbdf38b0148b792dd526aed5b6245a7e42252fcf
charlieflowers <cflowers@gmail.com>
2014-09-27 18:41:12 -0400

    ALL TESTS PASSING!

--- lex.rs
+++ lex.rs
@@ -224,7 +224,7 @@ impl<'li> Lexer<'li> {
         let mut cr = self.chomper.chomp_till_str(|str| str.starts_with("###")).unwrap();
         cr = delimiter + cr;
 
-        if cr.hitEof {
+        if ! cr.hitEof {
             cr = cr + self.chomper.expect("###");
         }
 
@@ -370,30 +370,32 @@ mod test {
                                                "[Whitespace  ]", "[Operator +]", "[Whitespace  ]", "[Number 40]"]);
     }
 
-//     #[test]
-//     fn should_handle_herecomments_correctly() {
-//         let code = r#"
-// 40 ### This whole thing right here is a
-// herecomment that can span
-// many lines. A # in the middle is no problem. It won't end until
-// the proper ending delimiter is encountered. ###"#;
+    #[test]
+    fn should_handle_herecomments_correctly() {
+        let code = r#"
+40 ### This whole thing right here is a
+herecomment that can span
+many lines. A # in the middle is no problem. It won't end until
+the proper ending delimiter is encountered. ###"#;
 
-//         let mut lexer = get_lexer(code);
-//         assert_tokens_match(&lexer.lex(), vec!["[Whitespace \n]", "[Number 40]", "[Whitespace  ]",
-//                                                "[Herecomment ### This whole thing right here is a\nherecomment that can span\nmany lines. A # in the middle is no problem. It won't end until\nthe proper ending delimiter is encountered. ###]"]);
-//     }
+        let mut lexer = get_lexer(code);
+        let tokens = lexer.lex();
+        assert_tokens_match(&lexer, &tokens, vec!["[Whitespace \n]", "[Number 40]", "[Whitespace  ]",
+                                               "[Herecomment ### This whole thing right here is a\nherecomment that can span\nmany lines. A # in the middle is no problem. It won't end until\nthe proper ending delimiter is encountered. ###]"]);
+    }
 
-//     #[test]
-//     fn should_handle_herecomments_that_hit_eof() {
-//         let code = r#"
-// 40 ### This whole thing right here is a
-// herecomment that
-// runs straight to EOF."#;
+    #[test]
+    fn should_handle_herecomments_that_hit_eof() {
+        let code = r#"
+40 ### This whole thing right here is a
+herecomment that
+runs straight to EOF."#;
 
-//         let mut lexer = get_lexer(code);
-//         assert_tokens_match(&lexer.lex(), vec!["[Whitespace \n]", "[Number 40]", "[Whitespace  ]",
-//                                                "[Herecomment ### This whole thing right here is a\nherecomment that\nruns straight to EOF.]"]);
-//     }
+        let mut lexer = get_lexer(code);
+        let tokens = lexer.lex();
+        assert_tokens_match(&lexer, &tokens, vec!["[Whitespace \n]", "[Number 40]", "[Whitespace  ]",
+                                               "[Herecomment ### This whole thing right here is a\nherecomment that\nruns straight to EOF.]"]);
+    }
 
     fn dump_tokens_to_console(code : Lexer, tokens: &Vec<Token> ) {
         let mut index :uint = 1;
